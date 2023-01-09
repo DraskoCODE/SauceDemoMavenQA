76ba92a1dc0c67c493ed87da0390b792eca00a7b
dragoje <dragojejanjevic@gmail.com>
2023-01-09 21:03:41 +0100

    Implement Products tests.

--- src/main/java/pages/ProductsPage.java
+++ src/main/java/pages/ProductsPage.java
@@ -1,7 +1,11 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class ProductsPage {
 
@@ -28,6 +32,53 @@ public class ProductsPage {
         driver.quit();
     }
 
+    public void addProductToCartByName(String productName) {
+        WebElement container = driver.findElement(By.id("inventory_container"));
 
+        List<WebElement> listInventoryItems = container.findElements(By.xpath(".//div[@class='inventory_item']"));
+
+        for(int i = 0; i < listInventoryItems.size(); i++) {
+            WebElement itemName = listInventoryItems.get(i).findElement(By.xpath(".//div[@class='inventory_item_name']"));
+            String itemNameText = itemName.getText();
+
+            if(itemNameText.equals(productName)) {
+                WebElement addButton = listInventoryItems.get(i).findElement(By.xpath(".//button"));
+                addButton.click();
+                break;
+            }
+        }
+    }
+
+    public void printProductPrice() {
+        WebElement container = driver.findElement(By.id("inventory_container"));
+
+        List<WebElement> listInventoryItems = container.findElements(By.xpath(".//div[@class='inventory_item']"));
+
+        for(int i = 0; i < listInventoryItems.size(); i++) {
+            WebElement itemPrice = listInventoryItems.get(i).findElement(By.xpath(".//div[@class='inventory_item_price']"));
+            System.out.println(itemPrice.getText());
+        }
+    }
+
+    public Integer productCountInCart() {
+        WebElement cartNumber = driver.findElement(By.xpath("//span[@class='shopping_cart_badge']"));
+        return Integer.parseInt(cartNumber.getText());
+    }
+
+    public void sortByValue(String sortValue) {
+        WebElement sortContainer = driver.findElement(By.xpath("//select[@data-test='product_sort_container']"));
+
+        sortContainer.click();
+
+        List<WebElement> options = sortContainer.findElements(By.xpath(".//option"));
+
+        for(int i = 0; i < options.size(); i++) {
+            String optionText = options.get(i).getText();
+            if(optionText.equals(sortValue)) {
+                options.get(i).click();
+                break;
+            }
+        }
+    }
 
 }
--- src/test/java/tests/ProductsTests.java
+++ src/test/java/tests/ProductsTests.java
@@ -1,4 +1,102 @@
 package tests;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.ProductsPage;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductsTests {
+
+    public static List<String> allItems() {
+        List<String> toReturn  = new ArrayList<>();
+
+        toReturn.add("Sauce Labs Bike Light");
+        toReturn.add("Sauce Labs Onesie");
+        toReturn.add("Sauce Labs Fleece Jacket");
+        toReturn.add("Sauce Labs Backpack");
+        toReturn.add("Sauce Labs Bolt T-Shirt");
+        toReturn.add("Test.allTheThings() T-Shirt (Red)");
+
+        return toReturn;
+    }
+
+    public static List<String> twoItems() {
+        List<String> toReturn  = new ArrayList<>();
+
+        toReturn.add("Sauce Labs Bike Light");
+        toReturn.add("Test.allTheThings() T-Shirt (Red)");
+
+        return toReturn;
+    }
+
+    @Test
+    public void verifyAddAllProductToCart() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\drago\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        loginPage.setUserName("standard_user");
+        loginPage.setPassword("secret_sauce");
+        loginPage.clickLogin();
+
+        ProductsPage productsPage = new ProductsPage(driver);
+        //productsPage.addProductToCartByName("Sauce Labs Bike Light"); //Sauce Labs Onesie
+        //productsPage.addProductToCartByName("Sauce Labs Onesie");
+        List<String> productToAdd = allItems();
+        for(int i = 0; i < productToAdd.size(); i++) {
+            productsPage.addProductToCartByName(productToAdd.get(i));
+        }
+
+        Assert.assertEquals(productsPage.productCountInCart(), productToAdd.size(), "Product count is not equals");
+
+        productsPage.close();
+    }
+
+    @Test
+    public void verifyAddTwoProductToCart() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\drago\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        loginPage.setUserName("standard_user");
+        loginPage.setPassword("secret_sauce");
+        loginPage.clickLogin();
+
+        ProductsPage productsPage = new ProductsPage(driver);
+        //productsPage.addProductToCartByName("Sauce Labs Bike Light"); //Sauce Labs Onesie
+        //productsPage.addProductToCartByName("Sauce Labs Onesie");
+        List<String> productToAdd = twoItems();
+        for(int i = 0; i < productToAdd.size(); i++) {
+            productsPage.addProductToCartByName(productToAdd.get(i));
+        }
+
+        Assert.assertEquals(productsPage.productCountInCart(), productToAdd.size(), "Product count is not equals");
+
+        productsPage.close();
+    }
+
+    @Test
+    public void verifySortProduct() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\drago\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.openPage();
+        loginPage.setUserName("standard_user");
+        loginPage.setPassword("secret_sauce");
+        loginPage.clickLogin();
+
+        ProductsPage productsPage = new ProductsPage(driver);
+        productsPage.sortByValue("Price (high to low)");
+        productsPage.printProductPrice();
+
+        productsPage.close();
+    }
+
 }
